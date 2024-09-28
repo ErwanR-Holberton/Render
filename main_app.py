3af51bd6b7509d3b1a3d0cb4bbef7b0848cf7f8c
ErwanR-Holberton <6218@holbertonstudents.com>
2024-09-28 17:07:13 +0200

    try fixing railway error

--- main_app.py
+++ main_app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def home():
-    return "Hello, Render!"
+def index():
+    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
